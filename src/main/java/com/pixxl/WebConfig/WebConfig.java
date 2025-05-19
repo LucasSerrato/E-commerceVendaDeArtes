027768bca3e37571146d2f82906de149b84933ec
Serenvilar <serenvilar.br@gmail.com>
2025-05-19 02:04:03 -0300

    WebConfig.java updated

--- src/main/java/com/pixxl/WebConfig/WebConfig.java
+++ src/main/java/com/pixxl/WebConfig/WebConfig.java
@@ -12,11 +12,7 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        Path uploadDir = Paths.get("uploads");
-        String uploadPath = uploadDir.toFile().getAbsolutePath();
-
         registry.addResourceHandler("/uploads/**")
-                .addResourceLocations("file:" + uploadPath + "/")
-                .setCachePeriod(3600);
+                .addResourceLocations("file:uploads/");
     }
 }
