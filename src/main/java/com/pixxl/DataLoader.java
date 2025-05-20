5a07a91269874e6d59d3b7514008173ad5627f94
Lucas S. <lucasserrato89@gmail.com>
2025-05-20 01:37:43 -0300

    mensagens sendo enviadas pressionando enter ou botão, antes só funcionava o botão corrigi.

--- src/main/java/com/pixxl/DataLoader.java
+++ src/main/java/com/pixxl/DataLoader.java
@@ -21,10 +21,12 @@ public class DataLoader implements CommandLineRunner {
             Cliente c1 = new Cliente(null, "Lucas", "lucas@gmail.com", "lucas123", false, null);
             Cliente c2 = new Cliente(null, "Ju", "ju@gmail.com", "ju123", true, null);
             Cliente c3 = new Cliente(null, "Pearls", "pearls@gmail.com", "pearls123", true, null);
+            Cliente c4 = new Cliente(null, "João", "joão@gmail.com", "joao123", true, null);
 
             clienteRepository.save(c1);
             clienteRepository.save(c2);
             clienteRepository.save(c3);
+            clienteRepository.save(c4);
 
             System.out.println("Usuários iniciais criados no banco.");
         } else {
